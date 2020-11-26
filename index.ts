2a71060759341887a00a0f818ae1c8e5f0f618a4
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2020-11-26 16:21:39 -0500

    Add getConcat

--- index.ts
+++ index.ts
@@ -26,7 +26,18 @@ class Cache {
         return undefined
     }
 
+    public async getConcat(...keys: string[]): Promise<Array<any>> {
+        let ret = []
+        for (let i = 0; i < keys.length; i++) {
+            let data = await this.get(keys[i])
+            if (typeof(data) !== 'array') {
+                throw new Error('Cache data must be an array to concat with getConcat')
+            }
+            ret = ret.concat(data)
+        }
 
+        return ret
+    }
 
     public bindMissHandler(key: string, lifetime: number, missHandler: CallableFunction) {
 
